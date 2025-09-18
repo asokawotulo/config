5a44ad5a7d308505922bf55fe8f808834a0c8d09
Asoka Wotulo <asokawotulo@yahoo.com>
2025-09-19 02:59:48 +0700

    fix(background): symlink to background image

--- setup.sh
+++ setup.sh
@@ -41,8 +41,8 @@ else
     echo "Brew is already installed, skipping installation..."
 fi
 
-rm "$HOME/Pictures/background.png"
-ln -s "$CONFIG_DIR/background.png" "$HOME/Pictures/background.png"
+rm -f "$HOME/Pictures/background.png"
+ln -sf "$CONFIG_DIR/background/background.png" "$HOME/Pictures/background.png"
 
 # Symlink git configuration files
 backup_and_link "$CONFIG_DIR/git/gitconfig" "$HOME/.gitconfig"
