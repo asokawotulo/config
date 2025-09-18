6bd1fd82e91a02cb92ac59fec10c507b42385d9a
Asoka Wotulo <asokawotulo@yahoo.com>
2025-09-19 02:17:57 +0700

    feat(nix): add mac app store apps

--- nix/flake.nix
+++ nix/flake.nix
@@ -111,9 +111,11 @@
           "zoom"
           "zen"
         ];
+        masApps = {
+          "Airmail - Lightning Fast Email" = 918858936;
+          "MoneyWiz 2025 Personal Finance" = 1511185140;
+        };
       };
-
-      # TODO: Add Mac App Store apps.
     };
   in
   {
